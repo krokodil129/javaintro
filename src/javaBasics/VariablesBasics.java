52e2a42bd5f6782afa24aa5883aa327410b4b5b7
david.karakash <david.karakash@AMS-GYM-EDV1-11.gym.ams.local>
2019-11-04 18:48:39 +0100

    Commit 4 VAriablen

---
+++ src/javaBasics/VariablesBasics.java
@@ -0,0 +1,28 @@
+package javaBasics;
+
+public class VariablesBasics {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean wahr_oder_falsch;
+		wahr_oder_falsch=false;
+		wahr_oder_falsch=true;
+		//
+		short ganz_kleine_zahl = 120;
+		System.out.println(ganz_kleine_zahl);
+		System.out.println(Short.MIN_VALUE);
+		System.out.println(Short.MAX_VALUE);
+		//
+		byte zahl=1;
+		System.out.println(zahl);
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+		//
+		int kleine_zahl = 123456;
+		System.out.println(kleine_zahl);
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+	}
+
+}
