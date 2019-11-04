3d45be24f5b48175805713d4c37f5cce39d543c6
david.karakash <david.karakash@AMS-GYM-EDV1-11.gym.ams.local>
2019-11-04 18:30:44 +0100

    commit 3 rechnen mit println und klammern

--- src/javaBasics/JavaBasics.java
+++ src/javaBasics/JavaBasics.java
@@ -9,13 +9,21 @@ public class JavaBasics {
 		System.out.print("du");
 		System.out.print(" ");
 		System.out.println("!");
-		
-		System.out.println("Hallo"+","+" "+"du"+" "+"!");
-		
+		//
+		System.out.println("Hallo" + "," + " " + "du" + " " + "!");
+		//
 		System.out.println(1.32);
-		System.out.println(123.45+12);
+		System.out.println(123.45 + 12);
 		System.out.println(12/4);
 		System.out.println(3*8);
+		System.out.println(8/3.0);
+		System.out.println(9/3D);
+		//
+		System.out.println(1 + 1 + "Ergebnis");
+		//
+		System.out.println("1 + 1 = " + 1 + 1);
+		System.out.println("1 + 1 = " + (1 + 1));
+		
 	}
 
 }
