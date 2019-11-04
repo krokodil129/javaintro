7781bb80b314c087b7952add67400f1303690e82
david.karakash <david.karakash@AMS-GYM-EDV1-11.gym.ams.local>
2019-11-04 18:16:25 +0100

    printline und rechnungen

---
+++ src/javaBasics/JavaBasics.java
@@ -0,0 +1,21 @@
+package javaBasics;
+
+public class JavaBasics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hallo,");
+		System.out.print(" ");
+		System.out.print("du");
+		System.out.print(" ");
+		System.out.println("!");
+		
+		System.out.println("Hallo"+","+" "+"du"+" "+"!");
+		
+		System.out.println(1.32);
+		System.out.println(123.45+12);
+		System.out.println(12/4);
+		System.out.println(3*8);
+	}
+
+}
