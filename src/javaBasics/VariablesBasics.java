979c35a578dd362fd6ff49690a5fcadca0cdf1d9
david.karakash <david.karakash@AMS-GYM-EDV1-11.gym.ams.local>
2019-11-11 17:43:23 +0100

    commit - long, float and double

--- src/javaBasics/VariablesBasics.java
+++ src/javaBasics/VariablesBasics.java
@@ -8,6 +8,7 @@ public class VariablesBasics {
 		boolean wahr_oder_falsch;
 		wahr_oder_falsch=false;
 		wahr_oder_falsch=true;
+		System.out.println(wahr_oder_falsch);
 		//
 		short ganz_kleine_zahl = 120;
 		System.out.println(ganz_kleine_zahl);
@@ -23,7 +24,24 @@ public class VariablesBasics {
 		System.out.println(kleine_zahl);
 		System.out.println(Integer.MIN_VALUE);
 		System.out.println(Integer.MAX_VALUE);
-		System.out.println('\u262d');
+		//
+		long grosse_zahl=1234567890;
+		System.out.println(grosse_zahl);
+		System.out.println(Long.MIN_VALUE);
+		System.out.println(Long.MAX_VALUE);
+		//
+		float kleiner_komma_zahl=123.123F; //ACHTUNG - OHNE F IST ES DOUBLE!!
+		System.out.println(kleiner_komma_zahl);
+		System.out.println(Float.MIN_VALUE);
+		System.out.println(Float.MAX_VALUE);
+		//
+		double grosser_komma_zahl=123456789.0987654321;
+		System.out.println(grosser_komma_zahl);
+		System.out.println(Double.MIN_VALUE);
+		System.out.println(Double.MAX_VALUE);
+		//
+		
+		
 	}
 
 }
