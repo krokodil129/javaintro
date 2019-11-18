46cc0f3e54c0776da205dd824cc2d438f174e30f
david.karakash <david.karakash@AMS-GYM-EDV1-11.gym.ams.local>
2019-11-18 18:54:14 +0100

    commit rechner 1

---
+++ src/aufgaben/AdditionsRechner.java
@@ -0,0 +1,17 @@
+package aufgaben;
+
+import java.util.Scanner;
+
+public class AdditionsRechner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		//
+		int z1=in.nextInt();
+		int z2=in.nextInt();
+		System.out.println(z1+z2);
+		
+	}
+
+}
