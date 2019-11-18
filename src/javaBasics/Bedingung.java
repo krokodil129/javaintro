be9523b400fd1da5b920a38559c6e06ad2e1164d
david.karakash <david.karakash@AMS-GYM-EDV1-11.gym.ams.local>
2019-11-18 17:23:55 +0100

    commit if #1

---
+++ src/javaBasics/Bedingung.java
@@ -0,0 +1,15 @@
+package javaBasics;
+
+public class Bedingung {
+
+	public static void main(String[] args) {
+		if (1 < 3) {
+			System.out.println("1 ist kleiner als 3");
+
+		}
+		if (3 < 1) {
+			System.out.println("3 ist kleiner als 1");
+		}
+	}
+
+}
