fa4131f85766a1fe218b5bda7ec5ead76a3d9bd9
david.karakash <david.karakash@AMS-GYM-EDV1-11.gym.ams.local>
2020-01-20 18:33:28 +0100

    with new numbers bubblesot

--- src/javaBasics/BubbleSort.java
+++ src/javaBasics/BubbleSort.java
@@ -1,23 +1,23 @@
 package javaBasics;
 
+import java.util.Arrays;
+
 public class BubbleSort {
 
 	public static void main(String[] args) {
-		int[] zahlen = { 1, 34, 5, 6, 9 };
-		for (int j = 0; j < zahlen.length - 1; j++) {  //Number of runs depends on array length
-			for (int i = 0; i < zahlen.length - 1; ++i) { //Every run makes checks between each pair of numbers
-				if (zahlen[i] > zahlen[i + 1]) { //If the first number is larger than the second, the numbers switch places
-					int temporary = zahlen[i]; 
+		int[] zahlen = { 9, 1, 5, 7, 4, 6, 3, 2, 8, 0 };
+		for (int j = 0; j < zahlen.length - 1; j++) { // Number of runs depends on array length
+			for (int i = 0; i < zahlen.length - 1; ++i) { // Every run makes checks between each pair of numbers
+				if (zahlen[i] > zahlen[i + 1]) { // If the first number is larger than the second, the numbers switch
+													// places
+					int temporary = zahlen[i];
 					zahlen[i] = zahlen[i + 1];
 					zahlen[i + 1] = temporary;
 				}
 			}
 		}
-		System.out.println(zahlen[0]); //Print out all the numbers in order
-		System.out.println(zahlen[1]);
-		System.out.println(zahlen[2]);
-		System.out.println(zahlen[3]);
-		System.out.println(zahlen[4]);
+		System.out.println(Arrays.toString(zahlen)); // Print out all the numbers in order
+
 	}
 
 }
