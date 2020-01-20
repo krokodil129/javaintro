59deec79185942ebfc733c0d2d6e036c5d48770b
david.karakash <david.karakash@AMS-GYM-EDV1-11.gym.ams.local>
2020-01-20 18:23:09 +0100

    with comments for bubble

--- src/javaBasics/BubbleSort.java
+++ src/javaBasics/BubbleSort.java
@@ -4,16 +4,16 @@ public class BubbleSort {
 
 	public static void main(String[] args) {
 		int[] zahlen = { 1, 34, 5, 6, 9 };
-		for (int j = 0; j < zahlen.length - 1; j++) {
-			for (int i = 0; i < zahlen.length - 1; ++i) {
-				if (zahlen[i] > zahlen[i + 1]) {
-					int temporary = zahlen[i];
+		for (int j = 0; j < zahlen.length - 1; j++) {  //Number of runs depends on array length
+			for (int i = 0; i < zahlen.length - 1; ++i) { //Every run makes checks between each pair of numbers
+				if (zahlen[i] > zahlen[i + 1]) { //If the first number is larger than the second, the numbers switch places
+					int temporary = zahlen[i]; 
 					zahlen[i] = zahlen[i + 1];
 					zahlen[i + 1] = temporary;
 				}
 			}
 		}
-		System.out.println(zahlen[0]);
+		System.out.println(zahlen[0]); //Print out all the numbers in order
 		System.out.println(zahlen[1]);
 		System.out.println(zahlen[2]);
 		System.out.println(zahlen[3]);
